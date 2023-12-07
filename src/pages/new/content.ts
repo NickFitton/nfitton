d00731fda0a4e93ea66640619baf0ecd35c037ce
Nick Fitton <nick.fitton@ovo.com>
2023-12-07 14:29:52 +0000

    Start redesigning homepage, add a christmas page

---
+++ src/pages/new/content.ts
@@ -0,0 +1,21 @@
+export const experience = [
+  {
+    company: 'OVO Energy',
+    from: '2021',
+    to: 'present',
+    content: [
+      'At OVO I’ve worn many hats, from junior React Native dev to interim Engineering Manager to Senior Platforms Lead. I’ve been able to work on a plethora of React based projects including React Native & NextJS.',
+      'During my tenure, I’ve successfully managed projects, built new integrations, and investigated innovative solutions to challenges in the energy industry.',
+    ],
+    tags: ['React Native', 'Next', 'TypeScript', 'Module Federation'],
+  },
+  {
+    company: 'Post-Quantum',
+    from: '2017',
+    to: '2021',
+    content: [
+      'Here I sharpened my software engineering skills, notably in quantum encryption, biometric security, and source-of-truth technology. Utilizing tools like Angular, Vue, Protractor, CyPress, Java Spring, Groovy, and Kubernetes, I developed innovative cybersecurity solutions.',
+      'This experience enhanced my ability to create secure, robust applications, contributing significantly to the company’s mission of protecting sensitive data and solidifying my foundation in the software engineering industry.',
+    ],
+  },
+];
